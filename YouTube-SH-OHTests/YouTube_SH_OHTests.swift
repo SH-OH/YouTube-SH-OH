71968b9948e867b778df543babdde2b17937109c
SH-OH <dhtkdgh429@gmail.com>
2020-09-14 23:29:55 +0900

    first commit

---
+++ YouTube-SH-OHTests/YouTube_SH_OHTests.swift
@@ -0,0 +1,34 @@
+//
+//  YouTube_SH_OHTests.swift
+//  YouTube-SH-OHTests
+//
+//  Created by Oh Sangho on 2020/09/14.
+//  Copyright © 2020 SH-OH. All rights reserved.
+//
+
+import XCTest
+@testable import YouTube_SH_OH
+
+class YouTube_SH_OHTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
